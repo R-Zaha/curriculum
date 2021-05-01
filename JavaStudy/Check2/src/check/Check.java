08ff516840392e058905576536b37e0a5d5e415a
“yitest00001” <yitest00001@gmail.com>
2021-05-01 16:08:33 +0900

    2章チェックテスト　再提出

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -2,17 +2,19 @@ package check;
 import constants.Constants;
 
 public class Check {
-	private String firstName="座覇";
-	private String lastName="音色";
+	private  static String firstName="座覇";
+	private  static String lastName="音色";
 
-	private void printName(String firstName, String lastName) {
+	private static void printName(String firstName, String lastName) {
 		System.out.println(firstName+ lastName);
 	}
 
 	public static void main(String[] args) {
-		Check check =new Check();
+		//Check check =new Check();
 		System.out.println("printNameメソッド→ ");
-		check.printName(check.firstName,check.lastName);
+		printName(firstName,lastName);
+		//再提出追記
+		//フィールドとprintNameメソッドをstaticにしたため、インスタンス化せずにprintNameメソッドを呼び出せるようになった。
 
 		Pet p =new Pet(Constants.CHECK_CLASS_JAVA,Constants.CHECK_CLASS_HOGE);
 		p.introduce();
